f84a14e4a98d11724b04ffac4e6246c03c19b7f4
qodirovoybekjon4929 <qodirovoybekjon08@gmail.com>
2022-10-21 17:10:06 +0500

    navite

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import navBar from "~/components/nav-bar.vue"
 </script>
 
 <template>
   <div>
-    <navBar message="hello word"/>
     <router-view/>
   </div>
 </template>
